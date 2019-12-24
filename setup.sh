e73ef208da438458a3e974acb767e394f266832e
J Kendal <13680617+joekendal@users.noreply.github.com>
2019-12-24 13:49:37 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,61 @@
-sudo apt update && sudo apt upgrade -y
+#!/bin/bash
 
-sudo apt install -y gnome-shell-extensions chrome-gnome-shell vanilla-gnome-desktop vim atom git tor autossh keepassxc nmap build-essential cmatrix irssi firejail
+sudo true
 
-sudo wget https://atom.io/download/deb -O /tmp/atom-amd64.deb && sudo apt install /tmp/atom-amd64.deb
+sudo sh -c 'echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1" > /etc/resolv.conf'
 
-apm install autocomplete-python python-tools python-indent
+# Harden GNOME defaults
+echo "[*] Updating settings"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
+echo "  Disabling microphone"
+gsettings set org.gnome.desktop.privacy disable-microphone true
+gsettings set org.gnome.desktop.privacy hide-identity true
+gsettings set org.gnome.desktop.privacy remember-app-usage false
+echo "  Disabling camera"
+gsettings set org.gnome.desktop.privacy disable-camera true
+gsettings set org.gnome.desktop.privacy remember-recent-files false
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true
+gsettings set org.gnome.desktop.privacy send-software-usage-stats false
+echo "  Disabling location"
+gsettings set org.gnome.system.location enabled false
+echo "  Disabling autorun media"
+gsettings set org.gnome.desktop.media-handling autorun-never true
+echo "  Disabling automount"
+gsettings set org.gnome.desktop.media-handling automount false
+gsettings set org.gnome.desktop.media-handling automount-open false
 
-echo "PS1='\[\e[0;37m\]\[\e[0m\]┌─[\[\e[0;96m\]\u\[\e[37m\]@\[\e[0m\]\h] \n└─[\[\e[1;37m\]\w\[\e[0m\]]> '" >> ~/.bashrc
+echo "[*] Updating packages"
+sudo dnf update -y
+echo "[+] Installing extra packages"
+sudo dnf install vim haveged firejail ghc-compiler tor keepassxc make clang ffmpeg -y
+echo "  Setting up firejail"
+sudo firecfg 1> /dev/null
+echo "[+] Installing Atom"
+wget https://atom.io/download/rpm --output-document=/tmp/atom.rpm
+sudo dnf install /tmp/atom.rpm -y
+shred -n 3 -u /tmp/atom.rpm
 
-echo "alias pdfjail='function jail() { firejail --seccomp --nonewprivs --private --private-dev --private-tmp --net=none --x11 --whitelist="$1" evince "$1"; }; jail'" >> ~/.bash_aliases
+echo "[+] Install Tor Browser"
+curl -s https://openpgpkey.torproject.org/.well-known/openpgpkey/torproject.org/hu/kounek7zrdx745qydx6p59t9mqjpuhdf | gpg --import -
+wget https://www.torproject.org/dist/torbrowser/9.0.1/tor-browser-linux64-9.0.1_en-US.tar.xz -O ~/Downloads/torbrowser-9.0.1.tar.xz
+wget https://dist.torproject.org/torbrowser/9.0.1/tor-browser-linux64-9.0.1_en-US.tar.xz.asc -O ~/Downloads/torbrowser-9.0.1.tar.xz.asc
+gpg --verify ~/Downloads/torbrowser-9.0.1.tar.xz.asc ~/Downloads/torbrowser-9.0.1.tar.xz
+if [ $? -eq -1 ]
+then
+	echo "  Problem with signature"
+	exit 1
+else
+sudo tar -xf ~/Downloads/torbrowser-9.0.1.tar.xz -C /opt/ && sudo chown -R joe /opt/tor-browser_en-US && sudo chgrp -R joe /opt/tor-browser_en-US && rm ~/Downloads/torbrowser-9.0.1.tar.xz
+cd /opt/tor-browser_en-US/
+./start-tor-browser.desktop --register-app
+fi
 
-echo ". ~/.bash_aliases" >> ~/.bashrc
+sudo wget -nv https://www.wireless.bris.ac.uk/certs/eaproot/uob-net-ca.pem --output-document=/etc/ssl/certs/UoB-Net-CA.pem
+
+sudo echo "7d9088c327577e1ccd9328db192f4abcec58dee0a513371c56d7c144ad68df33 */etc/ssl/certs/uob-net-ca.pem" | sha256sum -c -
+if [ $? -eq -1 ]
+then
+	echo "  Problem verifying certificate hash"
+	exit 1
+else
+
